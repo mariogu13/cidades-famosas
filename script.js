2572e742332447d61031d2492a707384b832da6f
Mário Gustavo <157504443+mariogu13@users.noreply.github.com>
2025-10-04 13:22:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,17 @@ function atualizarLista(){
         }
     });
 }
+
+const body = document.body; 
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const icon = darkModeToggle.querySelector("i");
+darkModeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (body.classList.contains("dark-mode")) {
+    icon.classList.remove("fa-moon");
+    icon.classList.add("fa-sun");
+  } else {
+    icon.classList.remove("fa-sun");
+    icon.classList.add("fa-moon");
+  }
+});
